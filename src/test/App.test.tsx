79b1b198dd18565b3bc992b0b1220ba34a6c3f56
marianarosa <mary.oliveira0212@gmail.com>
2024-08-05 16:32:40 -0300

    Configuração do teste para o componente App.tsx

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,26 +1,15 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
 
-describe('Test App Component', () => {
-  
-  beforeEach(() => {
+jest.mock('../components/Navbar', () => () => <div>Navbar</div>);
+jest.mock('../pages/BlogPost', () => () => <div>BlogPost Content</div>);
+
+describe('App Component', () => {
+  test('renders Navbar and BlogPost components', () => {
     render(<App />);
-  });
-
-  test('renders Vite + React heading', () => {
-    const headingElement = screen.getByText(/Vite \+ React/i);
-    expect(headingElement).toBeInTheDocument();
-  });
-
-  test('renders initial count correctly', () => {
-    const buttonElement = screen.getByText(/count is 0/i);
-    expect(buttonElement).toBeInTheDocument();
-  });
-
-  test('increments count when button is clicked', () => {
-    const buttonElement = screen.getByText(/count is 0/i);
-    fireEvent.click(buttonElement);
-    expect(buttonElement).toHaveTextContent(/count is 1/i);
+    
+    expect(screen.getByText(/Navbar/i)).toBeInTheDocument();
+    expect(screen.getByText(/BlogPost Content/i)).toBeInTheDocument();
   });
 });
