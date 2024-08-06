7cafaa13f99de77a5f8e8fd5e2f1db6691f4d1b0
marianarosa <mary.oliveira0212@gmail.com>
2024-08-06 02:59:56 -0300

    Criação do AvatarMap.ts para listagem de avatares

---
+++ src/utils/AvatarMap.ts
@@ -0,0 +1,17 @@
+import Avatar1 from '../assets/avatars/avatar1.jpg';
+import Avatar2 from '../assets/avatars/avatar2.jpg';
+import Avatar3 from '../assets/avatars/avatar3.jpg';
+import Avatar4 from '../assets/avatars/avatar4.jpg';
+import Avatar5 from '../assets/avatars/avatar5.jpg';
+import Avatar6 from '../assets/avatars/avatar6.jpg';
+
+const avatarMap: Record<number, string> = {
+  1: Avatar1,
+  2: Avatar2,
+  3: Avatar3,
+  4: Avatar4,
+  5: Avatar5,
+  6: Avatar6,
+};
+
+export default avatarMap;
